aad06cbaa5d6f5bfbb2fbcc5bfbc87e9606db345
40detectives <9024463+40detectives@users.noreply.github.com>
2021-12-04 01:54:59 +0100

    chore: add challenges #1, #2, #3, #4 & #5

--- iterator_challenges.js
+++ iterator_challenges.js
@@ -8,46 +8,61 @@
     A) Create a for loop that iterates through an array and returns the sum of the elements of the array.
     B) Create a functional iterator for an array that returns each value of the array when called, one element at a time. 
 */
+
 function sumFunc(arr) {
-    // YOUR CODE HERE
-    
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    return sum;
   }
   
+  
+  console.log("%c=== CHALLENGE 1 A) ===", "font-weight: bold; color: DeepPink");
   // Uncomment the lines below to test your work
-  // const array = [1, 2, 3, 4];
-  // console.log(sumFunc(array)); // -> should log 10
+  const array = [1, 2, 3, 4];
+  console.log(sumFunc(array)); // -> should log 10
   
   function returnIterator(arr) {
-    // YOUR CODE HERE
+    let i = 0;
+    return function iterator() {
+        return arr[i++];
+    }
   
   }
-  
+
+  console.log("%c=== CHALLENGE 1 B) ===", "font-weight: bold; color: DeepPink");
   // Uncomment the lines below to test your work
-  // const array2 = ['a', 'b', 'c', 'd'];
-  // const myIterator = returnIterator(array2);
-  // console.log(myIterator()); // -> should log 'a'
-  // console.log(myIterator()); // -> should log 'b'
-  // console.log(myIterator()); // -> should log 'c'
-  // console.log(myIterator()); // -> should log 'd'
-  
+  const array2 = ['a', 'b', 'c', 'd'];
+  const myIterator = returnIterator(array2);
+  console.log(myIterator()); // -> should log 'a'
+  console.log(myIterator()); // -> should log 'b'
+  console.log(myIterator()); // -> should log 'c'
+  console.log(myIterator()); // -> should log 'd'
   
   
   // CHALLENGE 2
   /*
     Create an iterator with a next method that returns each value of the array when .next is called.
   */
-  
-  function nextIterator(arr) {
-    // YOUR CODE HERE
-  
+    console.log("%c=== CHALLENGE 2 ===", "font-weight: bold; color: DeepPink");
+
+    function nextIterator(arr) {
+      let i = 0;
+      const iterator = {
+        next () {
+          return arr[i++];
+        }
+      }
+    return iterator;
   }
   
   // Uncomment the lines below to test your work
-  // const array3 = [1, 2, 3];
-  // const iteratorWithNext = nextIterator(array3);
-  // console.log(iteratorWithNext.next()); // -> should log 1
-  // console.log(iteratorWithNext.next()); // -> should log 2
-  // console.log(iteratorWithNext.next()); // -> should log 3
+  const array3 = [1, 2, 3];
+  const iteratorWithNext = nextIterator(array3);
+  console.log(iteratorWithNext.next()); // -> should log 1
+  console.log(iteratorWithNext.next()); // -> should log 2
+  console.log(iteratorWithNext.next()); // -> should log 3
   
   
   
@@ -55,16 +70,21 @@ function sumFunc(arr) {
   /*
     Write code to iterate through an entire array using your nextIterator and sum the values.
   */
+
+  console.log("%c=== CHALLENGE 3 ===", "font-weight: bold; color: DeepPink");
   
   function sumArray(arr) {
-    // YOUR CODE HERE
-    // use your nextIterator function
-  
+    const it = nextIterator(arr);
+    let sum = 0;
+    for (let current = it.next(); current !== undefined; current = it.next()) {
+        sum += current;
+    }
+    return sum;
   }
   
   // Uncomment the lines below to test your work
-  // const array4 = [1, 2, 3, 4];
-  // console.log(sumArray(array4)); // -> should log 10
+  const array4 = [1, 2, 3, 4];
+  console.log(sumArray(array4)); // -> should log 10
   
   
   
@@ -72,39 +92,53 @@ function sumFunc(arr) {
   /*
     Create an iterator with a next method that returns each value of a set when .next is called
   */
-  
+
+  console.log("%c=== CHALLENGE 4 ===", "font-weight: bold; color: DeepPink");
+
   function setIterator(set) {
-    // YOUR CODE HERE
-  
+    let i = 0;
+    const innerStorage = set.values();
+    return {
+        next: function() {
+            const nextIteration = innerStorage.next();
+            return nextIteration.value;
+        }
+    }
   }
   
   // Uncomment the lines below to test your work
-  // const mySet = new Set('hey');
-  // const iterateSet = setIterator(mySet);
-  // console.log(iterateSet.next()); // -> should log 'h'
-  // console.log(iterateSet.next()); // -> should log 'e'
-  // console.log(iterateSet.next()); // -> should log 'y'
-  
-  
+  const mySet = new Set('hey');
+  const iterateSet = setIterator(mySet);
+  console.log(iterateSet.next()); // -> should log 'h'
+  console.log(iterateSet.next()); // -> should log 'e'
+  console.log(iterateSet.next()); // -> should log 'y'
   
   // CHALLENGE 5
   /*
     Create an iterator with a next method that returns an array with two elements (where the first element is the index 
     and the second is the value at that index) when .next is called.
   */
+
+  console.log("%c=== CHALLENGE 5 ===", "font-weight: bold; color: DeepPink");
+
   function indexIterator(arr) {
-    // YOUR CODE HERE
+    let i = 0;
+    const innerStorage = [...arr];
+    return {
+        next() {
+            return [i, arr[i++]];
+        }
+    }
+    
   
   }
   
   // Uncomment the lines below to test your work
-  // const array5 = ['a', 'b', 'c', 'd'];
-  // const iteratorWithIndex = indexIterator(array5);
-  // console.log(iteratorWithIndex.next()); // -> should log [0, 'a']
-  // console.log(iteratorWithIndex.next()); // -> should log [1, 'b']
-  // console.log(iteratorWithIndex.next()); // -> should log [2, 'c']
-  
-  
+  const array5 = ['a', 'b', 'c', 'd'];
+  const iteratorWithIndex = indexIterator(array5);
+  console.log(iteratorWithIndex.next()); // -> should log [0, 'a']
+  console.log(iteratorWithIndex.next()); // -> should log [1, 'b']
+  console.log(iteratorWithIndex.next()); // -> should log [2, 'c']
   
   // CHALLENGE 6
   /*
